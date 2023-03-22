9f71b2e57f00561cd43f9ed3d526dd3cd1c3ef2f
Carolina zaraza <carolina.zaraza01@uceva.edu.co>
2023-03-21 20:03:40 -0500

    ejercicio pais_service

---
+++ pais_service-master/src/main/java/co/edu/uceva/pais_service/controller/PaisRestController.java
@@ -0,0 +1,45 @@
+package co.edu.uceva.pais_service.controller;
+
+import co.edu.uceva.pais_service.model.entities.Pais;
+import co.edu.uceva.pais_service.model.service.IPaisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/pais-service")
+public class PaisRestController {
+
+    @Autowired
+    IPaisService paisService;
+
+    /**
+     * Endpoint para recibir un saludo
+     * @param nombre Es el nombre que envian desde la url
+     * @return Un saludo
+     */
+    @GetMapping("/hola/{nombre}")
+    public String holaMundo(@PathVariable("nombre") String nombre){
+        return "Hola "+ nombre;
+    }
+
+    /**
+     * Metodo que lista los paises
+     * @return Lista de paises
+     */
+    @GetMapping("/paises")
+    public List<Pais> listar(){
+        return paisService.findAll();
+    }
+
+    @GetMapping("/paises/{id}")
+    public Pais buscarPais(@PathVariable("id") Long id){
+        return paisService.findById(id);
+    }
+
+}
---
+++ pais_service-master/src/main/java/co/edu/uceva/pais_service/model/service/PaisServiceImpl.java
@@ -0,0 +1,53 @@
+package co.edu.uceva.pais_service.model.service;
+
+import co.edu.uceva.pais_service.model.dao.IPaisDao;
+import co.edu.uceva.pais_service.model.entities.Pais;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Clase implementa el servicio de pais
+ * Un servicio es un objeto controlado por el contenedor de Spring
+ */
+@Service
+public class PaisServiceImpl implements IPaisService{
+
+    @Autowired
+    IPaisDao paisDao;
+
+    @Override
+    public Pais save(Pais pais) {
+        return null;
+    }
+
+    @Override
+    public void delete(Pais pais) {
+
+    }
+
+    @Override
+    public Pais update(Pais pais) {
+        return null;
+    }
+
+    /**
+     * Este metodo lista los paises guardados
+     * @return una lista de paises
+     */
+    @Override
+    public List<Pais> findAll() {
+        return (List<Pais>) paisDao.findAll();
+    }
+
+    /**
+     * Este metodo busca un pais
+     * @param id Llave primaria del pais que se quiere buscar
+     * @return el pais identificado por el campo id
+     */
+    @Override
+    public Pais findById(Long id) {
+        return paisDao.findById(id).get();
+    }
+}
